0196e10e4d8779be5e0278e57e37a95a2bc85095
samuelhulla <oficialny.hulla@gmail.com>
2021-02-19 12:18:10 +0100

    :green_circle: Feat: Material-UI Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
 import { NextComponentType, NextPageContext } from 'next'
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { ThemeProvider } from '@material-ui/core'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../lib/theme'
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  
+  // Server-side inject css clean-up
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
 
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Component {...pageProps} />
-) 
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Layout from '../components/Layout'
+import { Button } from '@material-ui/core'
 
 const Home = (): JSX.Element => (
   <Layout>
     <section>
       Home
+      <Button variant="contained" color="primary">
+        Test
+      </Button>
     </section>
   </Layout>
 )
